66ac7541133b31a0a1c6ee37dd28983843b941d3
Oswin Sorenson <sorens21@email.franklin.edu>
2025-10-25 13:40:31 -0400

    Loading PYPSA network system.

--- source/network.py
+++ source/network.py
@@ -1,9 +1,13 @@
 import ieee738
 import pandas
+import pypsa
 
 LINES_FILE="../data/hawaii40_osu/csv/lines.csv"
 LOADS_FILE="../data/hawaii40_osu/csv/loads.csv"
 GENS_FILE ="../data/hawaii40_osu/csv/generators.csv"
+BUSES_FILE="../data/hawaii40_osu/csv/buses.csv"
+TRANS_FILE="../data/hawaii40_osu/csv/transformers.csv"
+SHUNT_FILE="../data/hawaii40_osu/csv/shunt_impedances.csv"
 
 CDLIB = "../data/ieee738/conductor_library.csv"
 CDRAT = "../data/ieee738/conductor_ratings.csv"
@@ -28,15 +32,53 @@ class PartialConductorParams:
 
         
 class Network:
-    __slots__ = ["conductors", "lines", "loads", "generators"]
+    __slots__ = [
+        "subnet",
+        "conductors",
+        "buses",
+        "lines",
+        "loads",
+        "generators",
+        "transformers",
+        "shunts"
+    ]
 
     def __init__(self):
+        self.subnet = pypsa.Network()
         self.conductors = Conductors()
+        self.buses = pandas.read_csv(BUSES_FILE)
+        self.generators = pandas.read_csv(GENS_FILE)
         self.lines = pandas.read_csv(LINES_FILE)
         self.loads = pandas.read_csv(LOADS_FILE)
-        self.generators = pandas.read_csv(GENS_FILE)
+        self.transformers = pandas.read_csv(TRANS_FILE)
+        self.shunts = pandas.read_csv(SHUNT_FILE)
+        self.__load_subnet()
 
+    def __load_subnet(self):
+        for _, row in self.buses.iterrows():
+            self.subnet.add("Bus", **row)
+        
+        for _, row in self.generators.iterrows():
+            self.subnet.add("Generator", **row)
+
+        for _, row in self.lines.iterrows():
+            self.subnet.add("Line", **row)
+
+        for _, row in self.loads.iterrows():
+            self.subnet.add("Load", **row)
+
+        for _, row in self.transformers.iterrows():
+            self.subnet.add("Transformer", **row)
+            
+        for _, row in self.shunts.iterrows():
+            self.subnet.add("ShuntImpedance", **row)
+        
     def apply_atmospherics(self, **kwargs):
         params = PartialConductorParams(**kwargs)
-        
         pass
+
+    def solve(self):
+        self.subnet.optimize()
+        pass
+
+network = Network()
